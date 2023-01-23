2ff18e33a8b94d2011cea2b9622347643fc27f4d
rfrias8 <rfrias8@gmail.com>
2023-01-22 21:37:44 -0600

    add get jokes route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const { Joke } = require('./db');
+const { Op } = require("sequelize");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +9,20 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const where = {};
+    if (req.query.tags) {
+      where.tags = {
+        [Op.like]: `%${req.query.tags}%`,
+      };
+    }
+    if (req.query.content) {
+      where.joke = {
+        [Op.like]: `%${req.query.content}%`,
+      };
+    }
+
+    const jokes = await Joke.findAll({where});
+    
     res.send(jokes);
   } catch (error) {
     console.error(error);
